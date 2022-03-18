46ae332e720b018348120921919bfb99865b0f52
Sandra Willford <sandra@temdesign.us>
2022-03-18 14:33:08 -0400

    remove undefined stuff

--- sw.js
+++ sw.js
@@ -2,30 +2,10 @@ self.addEventListener('install', (event) => {
     self.skipWaiting()
         .then(() => console.log('Service Worker installed.'))
         .catch((err) => console.log(err));
-
-    if (SITE_TYPE === SiteType.PWA) {
-        event.waitUntil(
-            caches.open(CACHE_NAME).then(cache => {
-                return cache.addAll(CACHE_FILES);
-            })
-        );
-    }
 });
 
 self.addEventListener('activate', async (event) => {
     console.log(`Service worker activated.`);
-    event.waitUntil(self.clients.claim());
-    event.waitUntil(postMessageToClient({ serviceWorkerActivated: true }));
-    // Remove old caches
-    if (SITE_TYPE === SiteType.PWA) {
-        const keys = await caches.keys();
-        return keys.map(async (cache) => {
-            if (cache !== CACHE_NAME) {
-                console.log(`Service Worker: Removing old cache: ${cache}`);
-                return await caches.delete(cache);
-            }
-        });
-    }
 });
 
 async function handleNavigationRequest(event) {
