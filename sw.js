8152cad63cedebcce92a4a37058d072a359b6755
Sandra Willford <sandra@temdesign.us>
2022-03-18 14:14:22 -0400

    add sw to demo

---
+++ sw.js
@@ -0,0 +1,46 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting()
+        .then(() => console.log('Service Worker installed.'))
+        .catch((err) => console.log(err));
+
+    if (SITE_TYPE === SiteType.PWA) {
+        event.waitUntil(
+            caches.open(CACHE_NAME).then(cache => {
+                return cache.addAll(CACHE_FILES);
+            })
+        );
+    }
+});
+
+self.addEventListener('activate', async (event) => {
+    console.log(`Service worker activated.`);
+    event.waitUntil(self.clients.claim());
+    event.waitUntil(postMessageToClient({ serviceWorkerActivated: true }));
+    // Remove old caches
+    if (SITE_TYPE === SiteType.PWA) {
+        const keys = await caches.keys();
+        return keys.map(async (cache) => {
+            if (cache !== CACHE_NAME) {
+                console.log(`Service Worker: Removing old cache: ${cache}`);
+                return await caches.delete(cache);
+            }
+        });
+    }
+});
+
+self.addEventListener('fetch', async (event) => {
+    if (event.request.method === 'GET' && event.request.mode === 'navigate') {
+        const startIndexSearchTerm = '://';
+        const startIndex = event.request.url.indexOf(startIndexSearchTerm) + startIndexSearchTerm.length;
+        const path = event.request.url.substring(event.request.url.indexOf('/', startIndex));
+
+        if (!path.startsWith(INTERSTITIAL_REDIRECT_PREFIX)) event.respondWith(event);
+    }
+    else if (SITE_TYPE === SiteType.PWA && event.request.url.includes(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
