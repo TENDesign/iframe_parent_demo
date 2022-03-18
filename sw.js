691ba9531dc202749d8ff0985a8d82191c064a5e
Sandra Willford <sandra@temdesign.us>
2022-03-18 14:24:47 -0400

    add headers in sw

--- sw.js
+++ sw.js
@@ -28,13 +28,28 @@ self.addEventListener('activate', async (event) => {
     }
 });
 
+async function handleNavigationRequest(event) {
+
+    request = new Request(request, {
+        headers: {
+            ...request.headers,
+            refreshtoken: btoa(`refreshToken${new Date().getTime()}`),
+            accesstoken: btoa(`accessToken${new Date().getTime()}`)
+        }
+    });
+
+    return fetch(request);
+
+}
+
+
 self.addEventListener('fetch', async (event) => {
     if (event.request.method === 'GET' && event.request.mode === 'navigate') {
         const startIndexSearchTerm = '://';
         const startIndex = event.request.url.indexOf(startIndexSearchTerm) + startIndexSearchTerm.length;
         const path = event.request.url.substring(event.request.url.indexOf('/', startIndex));
 
-        if (!path.startsWith(INTERSTITIAL_REDIRECT_PREFIX)) event.respondWith(event);
+        if (!path.startsWith('/redirect')) event.respondWith(handleNavigationRequest(event));
     }
     else if (SITE_TYPE === SiteType.PWA && event.request.url.includes(self.location.origin)) {
         event.respondWith(
